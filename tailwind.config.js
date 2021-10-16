032bfc50fa039b8b5b53cd88323f76befce779af
Muhammad Sulaiman <sulaimanperson@gmail.com>
2021-10-16 22:52:49 +0500

    initail commit

---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+// tailwind.config.js
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+      extend: {
+        spacing: {
+          'wPanelDeskTop': '400px'
+        }
+       },
+  
+      screens: {
+        '2xl': {'max': '1535px'},
+        // => @media (max-width: 1535px) { ... }
+  
+        'xl': {'max': '1279px'},
+        // => @media (max-width: 1279px) { ... }
+  
+        'lg': {'max': '1023px'},
+        // => @media (max-width: 1023px) { ... }
+  
+        'md': {'max': '767px'},
+
+        'md2': {'max': '720px'},
+
+        // => @media (max-width: 767px) { ... }
+  
+        'sm': {'max': '639px'},
+
+        'sm2': {'max': '374px'},
+
+
+        
+
+      },
+      maxWidth: {
+        'newCutom': '1280px',
+        'paragraph': '1200px',
+
+       },
+       boxShadow: {
+       customShadow: '15px 0px 20px -9px rgba(0,0,0,0.55)'
+       
+      }
+       
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
