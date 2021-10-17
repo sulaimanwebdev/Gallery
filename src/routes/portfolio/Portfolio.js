51affa222abec1c1e5ec775051d2b26cb313b275
Muhammad Sulaiman <sulaimanperson@gmail.com>
2021-10-17 19:33:26 +0500

    ggg

--- src/routes/portfolio/Portfolio.js
+++ src/routes/portfolio/Portfolio.js
@@ -74,37 +74,37 @@ const [openBurger, setopenBurger] = useState("")
   <div class="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
     <nav class="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
       <div class="px-5 py-2">
-        <a href="#" class="text-base text-gray-500 hover:text-gray-900">
+        <a href="#" class="text-base font-bold  hover:text-gray-900">
           About
         </a>
       </div>
 
       <div class="px-5 py-2">
-        <a href="#" class="text-base text-gray-500 hover:text-gray-900">
+        <a href="#" class="text-base font-bold  hover:text-gray-900">
           Blog
         </a>
       </div>
 
       <div class="px-5 py-2">
-        <a href="#" class="text-base text-gray-500 hover:text-gray-900">
+        <a href="#" class="text-base font-bold  hover:text-gray-900">
           Jobs
         </a>
       </div>
 
       <div class="px-5 py-2">
-        <a href="#" class="text-base text-gray-500 hover:text-gray-900">
+        <a href="#" class="text-base font-bold  hover:text-gray-900">
           Press
         </a>
       </div>
 
       <div class="px-5 py-2">
-        <a href="#" class="text-base text-gray-500 hover:text-gray-900">
+        <a href="#" class="text-base font-bold  hover:text-gray-900">
           Accessibility
         </a>
       </div>
 
       <div class="px-5 py-2">
-        <a href="#" class="text-base text-gray-500 hover:text-gray-900">
+        <a href="#" class="text-base font-bold  hover:text-gray-900">
           Partners
         </a>
       </div>
@@ -145,7 +145,7 @@ const [openBurger, setopenBurger] = useState("")
         </svg>
       </a>
     </div>
-    <p class="mt-8 text-center text-base text-gray-400">
+    <p class="mt-8 text-center text-base opacity-70 font-light">
       &copy; 2020 Workflow, Inc. All rights reserved.
     </p>
   </div>
