dddc624eae6377d35c05197801d5775f35618f71
Muhammad Sulaiman <sulaimanperson@gmail.com>
2021-10-20 13:42:32 +0500

    d

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Portfolio from './routes/portfolio/Portfolio'
+import Home from './routes/Home'
+
 function App() {
   return (
     <>
     <BrowserRouter>
     <Switch>
+
+     <Route path="/" exact component={Home} />
       <Route path="/portfolio" component={Portfolio} />
+      
     </Switch>
     </BrowserRouter>
 
